9babaff2be7e921531a465ad3d212c3578615227
Ashmitha <ashmithan499@gmail.com>
2026-08-06 19:00:40 +0530

    Time: 0 ms (100%), Space: 9.8 MB (30.68%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
+long long ele(int i,int j) {
+    long long result=1;
+    for(int k=0;k<j;k++) {
+        result*=(i-k);
+        result/=k+1;
+    }
+    return result;
+}
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>v(numRows);
-        for(int i=0;i<numRows;i++) {
-            v[i]=vector<int>(i+1,0);
-        }
         for(auto i=0;i<numRows;i++) {
-            v[i][0]=1;
-            v[i][i]=1;
-        }
-        for(auto i=2;i<numRows;i++) {
-            for(auto j=1;j<i;j++) {
-                v[i][j]=v[i-1][j-1]+v[i-1][j];
+            v[i]=vector<int>(i+1);
+            for(auto j=0;j<i+1;j++) {
+                v[i][j]=ele(i,j);
             }
         }
         return v;
