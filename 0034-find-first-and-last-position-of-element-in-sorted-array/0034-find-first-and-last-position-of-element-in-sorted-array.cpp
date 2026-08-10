cba1f96cfed494b3e4881786130ea988d6a36119
Ashmitha <ashmithan499@gmail.com>
2026-08-10 08:38:32 +0530

    Time: 0 ms (100%), Space: 17.5 MB (54.59%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
+int lower(vector<int> &nums,int target) {
+    int low = 0,high = nums.size()-1;
+    while(low<=high) {
+        int mid = (low+high)/2;
+        if(nums[mid]>=target) {
+            high = mid-1;
+        }
+        else low = mid+1;
+    }
+     return low;
+}
+int upper(vector<int> &nums,int target) {
+     int low = 0,high = nums.size()-1;
+    while(low<=high) {
+        int mid = (low+high)/2;
+        if(nums[mid]>target) {
+            high = mid-1;
+        }
+        else low = mid+1;
+    }
+     return low;
+}
     vector<int> searchRange(vector<int>& nums, int target) {
-        int a = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        int b = upper_bound(nums.begin(),nums.end(),target)-nums.begin()-1;
+        int a = lower(nums,target);
+        int b = upper(nums,target);
         vector<int>v(2,-1);
-        if((a<nums.size() && nums[a]== target) && nums[b]==target) {
-           v[0]=a,v[1]=b;
+        if((a<nums.size() && nums[a]== target) && nums[b-1]==target) {
+           v[0]=a,v[1]=b-1;
         }
         return v;
     }
